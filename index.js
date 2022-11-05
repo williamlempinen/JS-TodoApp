b3638a7d4615747835b64dc10db947f58adc8e30
William Lempinen <wlempin@gmail.com>
2022-11-05 16:58:36 +0200

    5.11

--- index.js
+++ index.js
@@ -1,57 +1,40 @@
 let bttn = document.getElementById("submit-button");
-let todoList = document.getElementById("todos");
+let todoList = document.getElementById("todosUl");
+
+
 
 //crate a new list item when the button is clicked
 function addNewTodo() {
     let input = document.getElementById("input").value;
-    var i = document.createElement("li");
+    var liItem = document.createElement("li");
     var todo = document.createTextNode(input);
-    i.appendChild(todo);
+    liItem.appendChild(todo);
+    var liButton = document.createElement("button");
+    liButton.className = "closeButton";
+    liButton.innerHTML = "X";
+    liItem.appendChild(liButton);
+    liItem.id = "listItem";
     if (input === "") {
         alert("You have to write a todo to add it on the list!");
     } else {
-        todoList.appendChild(i);
+        todoList.appendChild(liItem);
+        document.getElementById("input").value = "";
+    } 
+    
+    let closeButton = document.getElementsByClassName("closeButton");
+    closeButton.onclick = function () {
+        var item = this.parentElement;
+        item.style.display = "none";
     }
-    document.getElementById("input").value = "";
-    var closeButton = document.createElement("span");
-    var text = document.createTextNode("\u00D7");
-    span.className = "close-button";
-    span.appendChild(text);
-    listOfItems.appendChild(span).join("");
-
-    for (i = 0; i < closeButton.length; i++) {
-        closeButton[i].onclick = () => {
-            var div = this.parentElement;
-            div.style.display = "none";
-        }
-      }
 }
 
 bttn.onclick = addNewTodo;
+clsBttn = document.getElementsByTagName("button");
 
-// close button and add it to every new todo-item
-let listOfItems = document.getElementsByTagName("li");
-var i;
-
-for (i = 0; i < listOfItems.length; i++) {
-    var span = document.createElement("span");
-    var text = document.createTextNode("\u00D7");
-    span.className = "close-button";
-    span.appendChild(text);
-    listOfItems[i].appendChild(span);
+clsBttn.onclick = function () {
+    var item = this.parentElement;
+    item.display = "none";
+    alert("moikka");
 }
-
-let closeButton = document.getElementsByClassName("close-button");
-
-//click on a close button to hide the current list item
-for (i = 0; i < closeButton.length; i++) {
-    closeButton[i].onclick = () => {
-        var div = this.parentElement;
-        div.style.display = "none";
-    }
-}
-
-//Add a "checked" symbol when cliking on a list item
-
 
 
